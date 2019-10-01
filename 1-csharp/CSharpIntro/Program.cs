92c4ed88cc4ff7fe9c6955e8b85ffa6aef4ae45c
Nicholas Escalona <nicholas.escalona@gmail.com>
2019-10-01 12:04:37 -0500

    add c# intro with some basic exercises.

---
+++ 1-csharp/CSharpIntro/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace CSharpIntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            // make bool, string, and int variables (with values)
+
+            // change their values to something else
+
+            // print their values with Console
+
+            // do something in a for loop
+            // do something in a while loop
+            // do something with a switch statement
+
+            // do something with if, else if, else
+
+            // figure out how to make multi-line comments in C#
+            // figure out how to format your document in VS Code
+
+            // extra: make a new static method to do something and call it
+            // extra: learn what "var" means in C# and try it out.
+        }
+    }
+}
