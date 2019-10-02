4d44f9eb08c67630e1eee725a033e560c4c2e8cd
Nicholas Escalona <nicholas.escalona@gmail.com>
2019-10-02 15:11:20 -0500

    inheritance example.

--- 1-csharp/ShapesApp/ShapesApp.Library/Circle.cs
+++ 1-csharp/ShapesApp/ShapesApp.Library/Circle.cs
@@ -4,6 +4,13 @@ namespace ShapesApp.Library
 {
     public class Circle : IShape
     {
+        // constructor
+        public Circle(double radius)
+        {
+            Radius = radius;
+            // now there can't be a circle without setting the radius
+        }
+
         public int Dimensions => 2;
         public int Sides => 0;
 
@@ -13,6 +20,8 @@ namespace ShapesApp.Library
 
         // "expression-body syntax" for methods
         // just a shorter way to write a one-line method with a return.
-        public double GetPerimeter() => 2 * Math.PI * Radius;
+        public virtual double GetPerimeter() => 2 * Math.PI * Radius;
+
+        // non0virtual methods cannot be overridden
     }
 }
