1f042614ff64569edb72f2771ca75cc0fddcf888
Nicholas Escalona <nicholas.escalona@gmail.com>
2019-10-03 14:52:59 -0500

    json

--- 1-csharp/Serialization/Serialization/Address.cs
+++ 1-csharp/Serialization/Serialization/Address.cs
@@ -1,9 +1,14 @@
-﻿namespace Serialization
+﻿using Newtonsoft.Json;
+
+namespace Serialization
 {
     public class Address
     {
+        [JsonProperty("StreetAddress")]
         public string Street { get; set; }
         public string City { get; set; }
+
+        [JsonIgnore]
         public string State { get; set; }
     }
 }
--- 1-csharp/Serialization/Serialization/Program.cs
+++ 1-csharp/Serialization/Serialization/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Xml.Serialization;
+using Newtonsoft.Json;
 
 namespace Serialization
 {
@@ -12,14 +13,37 @@ namespace Serialization
             // "" strings you need to escape some characters with \
             // @"" strings you don't
             var xmlFilePath = @"C:\revature\persons.xml";
+            var jsonFilePath = @"C:\revature\persons.json";
 
             //var data = GetInitialData();
 
-            var data = DeserializeXmlFromFile(xmlFilePath);
+            //var data = DeserializeXmlFromFile(xmlFilePath);
+            var data = DeserializeJsonFromFile(jsonFilePath);
 
             ModifyData(data);
 
-            SerializeXmlToFile(xmlFilePath, data);
+            //SerializeXmlToFile(xmlFilePath, data);
+            SerializeJsonToFile(jsonFilePath, data);
+        }
+
+        public static void SerializeJsonToFile(string jsonFilePath, List<Person> data)
+        {
+            // we will do this with JSON.NET aka Newtonsoft Json
+            // we use NuGet to get these dependencies
+            string json = JsonConvert.SerializeObject(data);
+
+            // exceptions should be handled here, ignored for sake of time
+            File.WriteAllText(jsonFilePath, json);
+        }
+
+        public static List<Person> DeserializeJsonFromFile(string jsonFilePath)
+        {
+            // exceptions should be handled here, ignored for sake of time
+            string json = File.ReadAllText(jsonFilePath);
+
+            var data = JsonConvert.DeserializeObject<List<Person>>(json);
+
+            return data;
         }
 
         public static void ModifyData(List<Person> data)
@@ -67,11 +91,21 @@ namespace Serialization
             // XmlSerializer was made pre-generics and has not been updated
             var serializer = new XmlSerializer(typeof(List<Person>));
 
-            FileStream fileStream = null;
+            // "using statement" replaces a try-finally-dispose on an disposable object.
 
             try
             {
-                fileStream = new FileStream(xmlFilePath, FileMode.Create);
+                //using (fileStream = new FileStream(xmlFilePath, FileMode.Create))
+                //{
+                //    serializer.Serialize(fileStream, data);
+
+                //    // at the end of the using block, the object is disposed automatically (regardless
+                //    // of any unhandled exceptions)
+                //}
+
+                // from c# 8, we have "using declaration" - instead of having to indent a whole block,
+                // the implicit dispose will happen at the end of the current block
+                using var fileStream = new FileStream(xmlFilePath, FileMode.Create);
 
                 serializer.Serialize(fileStream, data);
             }
@@ -82,13 +116,6 @@ namespace Serialization
             catch (InvalidOperationException ex)
             {
                 Console.WriteLine($"Error while serializing: {ex.Message}");
-            }
-            finally // finally block always runs whether, no-exception, handled-exception, or unhandled-exception
-            {
-                if (fileStream != null)
-                {
-                    fileStream.Dispose();
-                }
             }
         }
 
