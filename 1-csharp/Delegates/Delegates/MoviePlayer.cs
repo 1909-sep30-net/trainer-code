a4eaa4d4e0b535dc130fe04c086176263ef13bfe
Nicholas Escalona <nicholas.escalona@gmail.com>
2019-10-03 15:47:10 -0500

    set up delegate & event demo

---
+++ 1-csharp/Delegates/Delegates/MoviePlayer.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace Delegates
+{
+    public class MoviePlayer
+    {
+        public string CurrentMovie { get; set; }
+
+        // events are entities that work on a publish-subscribe idea
+        // C# lets you subscribe event handler delegates to events.
+
+        // the type for handling the event:
+        public delegate void MovieFinishedHandler();
+        // defines a delegate type for void-return and zero-parameters.
+        // this describes the shape of functions that can subscribe to this event i'm about to define.
+
+        // the event member, one distinct event per instance of this class
+        public event MovieFinishedHandler MovieFinished;
+
+        public void PlayMovie()
+        {
+            Console.WriteLine($"Playing movie {CurrentMovie}");
+
+            Thread.Sleep(3000); // wait for 3 seconds
+
+            // fire the event. (the syntax looks just like calling a method)
+            // what it effectively does is call all the subscribing delegates
+
+            // weird issue - if there are no subscribers, the event is kind of "null"
+            //if (MovieFinished != null)
+            //{
+            //    MovieFinished();
+            //}
+            MovieFinished?.Invoke(); // does same thing as above code - do nothing if no subscribers
+        }
+    }
+}
---
+++ 1-csharp/Delegates/Delegates/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Delegates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var movie = "Frozen";
+
+            var moviePlayer = new MoviePlayer { CurrentMovie = movie };
+
+            moviePlayer.PlayMovie();
+        }
+    }
+}
