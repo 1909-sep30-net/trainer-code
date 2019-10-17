db71d4770c592ca5d39cd7b612910cad004e429a
Nicholas Escalona <nicholas.escalona@gmail.com>
2019-10-17 11:12:26 -0500

    bunch of mvc stuff.

---
+++ 3-mvc/HelloMvc/HelloMvc/Controllers/CustomerController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using HelloMvc.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HelloMvc.Controllers
+{
+    [Route("accounts")] // example of "attribute routing"
+    // alternative to endpoint routing, configured globally in the Startup file
+    // endpoint routing is preferred for MVC apps. ()
+    public class CustomerController : Controller
+    {
+        // this is an action method
+        // each action method is going to correspond to one "URL" for our website
+        //    (if there are parameters in that URL, we can handle many values in one action method)
+        public IActionResult Index()
+        {
+            // every action method's job is to fetch data / push data / whatever
+            // "action" is represented by this URL that the user is sending a request to
+
+            // and then return some "result"
+            //     a result is something that ASP.NET Core can "render" into an HTTP response.
+            //    within the MVC world, our results are either ViewResult or some kind of RedirectResult
+
+            // views are the kind of result that renders into HTML.
+            // "View()" is a helper method from the base class that looks for
+            // a view matching the name of this current action method ("Index"),
+            //      for this controller ("Customer").
+            var customers = new List<Customer>
+            {
+                new Customer { Id = 1, Name = "Nick Enscala" },
+                new Customer { Id = 2, Name = "Fred Belotte" }
+            };
+
+            //DynamicTests();
+
+            return View(customers); // this is how you give a view its model
+        }
+
+        //[NonAction] // tells MVC this is not an action method
+        //private void DynamicTests()
+        //{
+        //    dynamic dynamicValue = 45;
+        //    dynamicValue.Name = "abc";
+        //}
+    }
+}
--- 3-mvc/HelloMvc/HelloMvc/Startup.cs
+++ 3-mvc/HelloMvc/HelloMvc/Startup.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -18,19 +18,27 @@ namespace HelloMvc
             Configuration = configuration;
         }
 
+        // this Configuration object is for reading in runtime configuration
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            // in here, we register services for dependency injection
+            // we set up middleware in here
+
             services.AddControllersWithViews();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            // in here we "plug in" middleware to the request lifecycle
+            // and at the bottom, configure our routes.
+
             if (env.IsDevelopment())
             {
+                // if Dev environment, plug in that nice exception page we saw
                 app.UseDeveloperExceptionPage();
             }
             else
@@ -48,6 +56,22 @@ namespace HelloMvc
 
             app.UseEndpoints(endpoints =>
             {
+                // in this lambda expression here,
+                // we can map as many routes as we want
+                // right now one route is mapped
+
+                // the routes are matched against each incoming URL,
+                // the first one here that matches it will be followed
+                endpoints.MapControllerRoute(
+                    name: "privacy",
+                    pattern: "Privacy", // the pattern is matched against the URL.
+                    defaults: new { Controller = "Home", Action = "Privacy" });
+                // that is "anonymous type" - an object without a class.
+                // "web developer" way of thinking, just stuff the data in there
+                // with no compile-time type checking
+
+                // this is "endpoint routing"
+                // the alternative is "attribute routing"
                 endpoints.MapControllerRoute(
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
