d50a5375216c82deb05a1a56aa4fa13ffe09f977
Nicholas Escalona <nicholas.escalona@gmail.com>
2019-10-23 15:33:23 -0500

    add console with build inside docker

---
+++ 4-devops/ContainerizedConsoleBuild/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ContainerizedConsoleBuild
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
