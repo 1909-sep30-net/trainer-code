1cc32057998f6f57d13539df8ef32068de31be6d
Nicholas Escalona <nicholas.escalona@gmail.com>
2019-11-04 13:55:41 -0600

    add more partial ajax

--- 6-angular/2-js-ajax/index.js
+++ 6-angular/2-js-ajax/index.js
@@ -14,13 +14,38 @@
 
 document.addEventListener('DOMContentLoaded', function () {
     let button = document.getElementById('jokeButton');
+    let display = document.getElementById('jokeDisplay');
 
     button.addEventListener('click', function () {
         // make request to api
         let xhr = new XMLHttpRequest();
 
         xhr.addEventListener('readystatechange', function () {
-            // "readyState" property
-        })
+            // "xhr.readyState" property indicates
+            // progress through request-response cycle
+            // 4 means, response is loaded
+            if (xhr.readyState === 4) {
+                if (xhr.status >= 200 && xhr.status < 300) {
+                    // success; the "xhr.response" property
+                    // now has the response body.
+                    // if the response body is in JSON...
+                    // we have JSON serializer
+                    // - JSON.stringify() serializes
+                    // - JSON.parse() deserializes
+
+                    // task: get the joke
+                    let joke = '';
+                    display.innerHTML = joke;
+                } else {
+                    // display error to user
+                    console.error('http error');
+                    console.log(xhr.response);
+                }
+            }
+        });
+
+        xhr.open('(method goes here)', '(url goes here)');
+
+        xhr.send();
     });
 });
