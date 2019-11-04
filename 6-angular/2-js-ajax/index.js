3950e35fac41c0141691368b3294a144eca80a89
Nicholas Escalona <nicholas.escalona@gmail.com>
2019-11-04 14:19:11 -0600

    code calls api

--- 6-angular/2-js-ajax/index.js
+++ 6-angular/2-js-ajax/index.js
@@ -17,35 +17,41 @@ document.addEventListener('DOMContentLoaded', function () {
     let display = document.getElementById('jokeDisplay');
 
     button.addEventListener('click', function () {
-        // make request to api
-        let xhr = new XMLHttpRequest();
-
-        xhr.addEventListener('readystatechange', function () {
-            // "xhr.readyState" property indicates
-            // progress through request-response cycle
-            // 4 means, response is loaded
-            if (xhr.readyState === 4) {
-                if (xhr.status >= 200 && xhr.status < 300) {
-                    // success; the "xhr.response" property
-                    // now has the response body.
-                    // if the response body is in JSON...
-                    // we have JSON serializer
-                    // - JSON.stringify() serializes
-                    // - JSON.parse() deserializes
-
-                    // task: get the joke
-                    let joke = '';
-                    display.innerHTML = joke;
-                } else {
-                    // display error to user
-                    console.error('http error');
-                    console.log(xhr.response);
-                }
-            }
-        });
-
-        xhr.open('(method goes here)', '(url goes here)');
-
-        xhr.send();
+        getJokeWithXhr(display);
     });
 });
+
+function getJokeWithXhr(display) {
+    // make request to api
+    let xhr = new XMLHttpRequest();
+
+    xhr.addEventListener('readystatechange', function () {
+        // "xhr.readyState" property indicates
+        // progress through request-response cycle
+        // 4 means, response is loaded
+        if (xhr.readyState === 4) {
+            if (xhr.status >= 200 && xhr.status < 300) {
+                // success; the "xhr.response" property
+                // now has the response body.
+                // if the response body is in JSON...
+                // we have JSON serializer
+                // - JSON.stringify() serializes
+                // - JSON.parse() deserializes
+
+                // task: get the joke
+                let obj = JSON.parse(xhr.response);
+                console.log(obj);
+                let joke = obj.value.joke;
+                display.innerHTML = joke;
+            } else {
+                // display error to user
+                console.error('http error');
+                console.log(xhr.response);
+            }
+        }
+    });
+
+    xhr.open('get', 'http://api.icndb.com/jokes/random');
+
+    xhr.send();
+}
