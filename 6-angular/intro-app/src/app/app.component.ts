57f6231798b3ad66fd47d97ac5fda8d5a5d8c451
Nicholas Escalona <nicholas.escalona@gmail.com>
2019-11-07 10:14:39 -0600

    add ng material and g fridge component

--- 6-angular/intro-app/src/app/app.component.ts
+++ 6-angular/intro-app/src/app/app.component.ts
@@ -1,9 +1,30 @@
 import { Component } from '@angular/core';
 
 @Component({
+  // the "selector" (think CSS selector)
+  // tells angular what elements in HTML to replace with
+  // this component
   selector: 'app-root',
+  // here we connect the component TS class (logic) to
+  // the HTML template (display/view)
+  // (you can also use "inline template" with template property)
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  // template: `
+  //   <div>
+  //   </div>
+  // `,
+
+  // in angular, CSS is encapsulated with each component.
+  // you are free to write that css as though that component
+  // were the whole world
+  // and we can have several in this array
+  styleUrls: ['./app.component.css'],
+  // inline styles are also an option
+  // styles: [
+  //   `div { color: black }`
+  // ]
+
+  // we also have "animations" to configure here
 })
 export class AppComponent {
   title = 'intro-app';
--- 6-angular/intro-app/src/app/app.module.ts
+++ 6-angular/intro-app/src/app/app.module.ts
@@ -2,15 +2,36 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FridgeComponent } from './fridge/fridge.component';
 
+// this is a decorator
+// decorators are preview TS syntax
+// Angular defines several important decorators
+// whose job is to tell angular to turn
+// a given TS class into a special angular thing:
+//   module - NgModule
+//   component - Component
+//   services - Injectable
+//   directive - Directive
 @NgModule({
+  // every component must be declared in exactly one module
   declarations: [
-    AppComponent
+    AppComponent,
+    FridgeComponent
   ],
+  // if anything in this module needs anything from another module,
+  // we have to reference that second module here.
+  // we know about "TS import" that connects TS modules
+  // this is "NG import" that connect NG modules
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule
   ],
+  // we can register services for DI here (scoped to this module)
   providers: [],
+  // only the root module needs this entry
+  // and it should contain the root component
   bootstrap: [AppComponent]
 })
 export class AppModule { }
